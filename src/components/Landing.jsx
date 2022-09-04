8284bd41e3169f7758c5ad6e834b60113de638bd
el634dev <84924260+el634dev@users.noreply.github.com>
2022-09-04 18:40:49 -0400

    Update Landing.jsx

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -19,12 +19,10 @@ import LanguagesDropdown from "./LanguagesDropdown";
 const javascriptDefault = `/**
 * Problem: Binary Search: Search a sorted array for a target value.
 */
-
 // Time: O(log n)
 const binarySearch = (arr, target) => {
  return binarySearchHelper(arr, target, 0, arr.length - 1);
 };
-
 const binarySearchHelper = (arr, target, start, end) => {
  if (start > end) {
    return false;
@@ -40,7 +38,6 @@ const binarySearchHelper = (arr, target, start, end) => {
    return binarySearchHelper(arr, target, start, mid - 1);
  }
 };
-
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const target = 5;
 console.log(binarySearch(arr, target));
@@ -62,7 +59,7 @@ const Landing = () => {
     setLanguage(sl);
   };
 
-  useEffect((handleCompile) => {
+  useEffect(() => {
     if (enterPress && ctrlPress) {
       console.log("enterPress", enterPress);
       console.log("ctrlPress", ctrlPress);
@@ -80,7 +77,6 @@ const Landing = () => {
       }
     }
   };
-
   const handleCompile = () => {
     setProcessing(true);
     const formData = {
@@ -141,7 +137,7 @@ const Landing = () => {
       let response = await axios.request(options);
       let statusId = response.data.status?.id;
 
-      // Processed
+      // Processed - we have a result
       if (statusId === 1 || statusId === 2) {
         // still processing
         setTimeout(() => {
@@ -190,7 +186,7 @@ const Landing = () => {
     });
   };
   const showErrorToast = (msg, timer) => {
-    toast.error(msg || `Something's not right! Please try again.`, {
+    toast.error(msg || `Something went wrong! Please try again.`, {
       position: "top-right",
       autoClose: timer ? timer : 1000,
       hideProgressBar: false,
@@ -215,7 +211,36 @@ const Landing = () => {
         pauseOnHover
       />
 
-    <div className="h-4 w-full bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500"></div>
+      <a
+        href="https://github.com/manuarora700/react-code-editor"
+        title="Fork me on GitHub"
+        class="github-corner"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <svg
+          width="50"
+          height="50"
+          viewBox="0 0 250 250"
+          className="relative z-20 h-20 w-20"
+        >
+          <title>Fork me on GitHub</title>
+          <path d="M0 0h250v250"></path>
+          <path
+            d="M127.4 110c-14.6-9.2-9.4-19.5-9.4-19.5 3-7 1.5-11 1.5-11-1-6.2 3-2 3-2 4 4.7 2 11 2 11-2.2 10.4 5 14.8 9 16.2"
+            fill="currentColor"
+            style={{ transformOrigin: "130px 110px" }}
+            class="octo-arm"
+          ></path>
+          <path
+            d="M113.2 114.3s3.6 1.6 4.7.6l15-13.7c3-2.4 6-3 8.2-2.7-8-11.2-14-25 3-41 4.7-4.4 10.6-6.4 16.2-6.4.6-1.6 3.6-7.3 11.8-10.7 0 0 4.5 2.7 6.8 16.5 4.3 2.7 8.3 6 12 9.8 3.3 3.5 6.7 8 8.6 12.3 14 3 16.8 8 16.8 8-3.4 8-9.4 11-11.4 11 0 5.8-2.3 11-7.5 15.5-16.4 16-30 9-40 .2 0 3-1 7-5.2 11l-13.3 11c-1 1 .5 5.3.8 5z"
+            fill="currentColor"
+            class="octo-body"
+          ></path>
+        </svg>
+      </a>
+
+      <div className="h-4 w-full bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500"></div>
       <div className="flex flex-row">
         <div className="px-4 py-2">
           <LanguagesDropdown onSelectChange={onSelectChange} />
@@ -224,7 +249,7 @@ const Landing = () => {
           <ThemeDropdown handleThemeChange={handleThemeChange} theme={theme} />
         </div>
       </div>
-       <div className="flex flex-row space-x-4 items-start px-4 py-4">
+      <div className="flex flex-row space-x-4 items-start px-4 py-4">
         <div className="flex flex-col w-full h-full justify-start items-end">
           <CodeEditorWindow
             code={code}
@@ -232,11 +257,8 @@ const Landing = () => {
             language={language?.value}
             theme={theme.value}
           />
-        </div>  
-     </div>
-      
-      <div className="right-container flex flex-shrink-0 w-[30%] flex-col">
-        <OutputWindow outputDetails={outputDetails} />
+        </div>
+
         <div className="right-container flex flex-shrink-0 w-[30%] flex-col">
           <OutputWindow outputDetails={outputDetails} />
           <div className="flex flex-col items-end">
@@ -262,4 +284,5 @@ const Landing = () => {
     </>
   );
 };
+
 export default Landing;

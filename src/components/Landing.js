194aa4cd5088a2e519b7d9c9d7fa3d037d5d51cc
el634dev <84924260+el634dev@users.noreply.github.com>
2022-09-05 16:20:26 -0400

    Update and rename Landing.jsx to Landing.js

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -210,7 +210,7 @@ const Landing = () => {
         draggable
         pauseOnHover
       />
-
+       /*
       <a
         href="https://github.com/el634dev/react-editor"
         title="Fork me on GitHub"
@@ -218,12 +218,14 @@ const Landing = () => {
         target="_blank"
         rel="noreferrer"
       >
+      
         <svg
           width="50"
           height="50"
           viewBox="0 0 250 250"
           className="relative z-20 h-20 w-20"
         >
+         
           <title>Fork on GitHub</title>
           <path d="M0 0h250v250"></path>
           <path
@@ -232,6 +234,7 @@ const Landing = () => {
             style={{ transformOrigin: "130px 110px" }}
             class="octo-arm"
           ></path>
+   
           <path
             d="M113.2 114.3s3.6 1.6 4.7.6l15-13.7c3-2.4 6-3 8.2-2.7-8-11.2-14-25 3-41 4.7-4.4 10.6-6.4 16.2-6.4.6-1.6 3.6-7.3 11.8-10.7 0 0 4.5 2.7 6.8 16.5 4.3 2.7 8.3 6 12 9.8 3.3 3.5 6.7 8 8.6 12.3 14 3 16.8 8 16.8 8-3.4 8-9.4 11-11.4 11 0 5.8-2.3 11-7.5 15.5-16.4 16-30 9-40 .2 0 3-1 7-5.2 11l-13.3 11c-1 1 .5 5.3.8 5z"
             fill="currentColor"
@@ -239,7 +242,7 @@ const Landing = () => {
           ></path>
         </svg>
       </a>
-
+      */
       <div className="h-4 w-full bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500"></div>
       <div className="flex flex-row">
         <div className="px-4 py-2">
